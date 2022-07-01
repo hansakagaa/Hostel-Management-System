5ad12e5188d587b07e42501a5eb58da395d4311f
Hasii-boy <hansakagaa@gmail.com>
2022-07-02 01:11:03 +0530

    UserPasswordDAOImpl created

--- Hostel_Management_System/src/lk/ijse/Hibernate/dao/custom/impl/User_passwordDAOImpl.java
+++ Hostel_Management_System/src/lk/ijse/Hibernate/dao/custom/impl/User_passwordDAOImpl.java
@@ -2,8 +2,12 @@ package lk.ijse.Hibernate.dao.custom.impl;
 
 import lk.ijse.Hibernate.dao.custom.User_passwordDAO;
 import lk.ijse.Hibernate.entity.User_password;
+import lk.ijse.Hibernate.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
     @author : Hasii-boy
@@ -11,31 +15,73 @@ import java.util.ArrayList;
 public class User_passwordDAOImpl implements User_passwordDAO {
     @Override
     public boolean exits(String s) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+
+        transaction.commit();
+        session.close();
         return false;
     }
 
     @Override
-    public User_password find(String s) throws Exception {
-        return null;
+    public User_password find(String user) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        User_password userPassword = session.find(User_password.class, user);
+
+        transaction.commit();
+        session.close();
+        return userPassword;
     }
 
     @Override
     public ArrayList<User_password> getAll() throws Exception {
-        return null;
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<User_password> userPassword = session.createQuery("from User_password ").list();
+
+        transaction.commit();
+        session.close();
+        return (ArrayList<User_password>) userPassword;
     }
 
     @Override
-    public boolean save(User_password dto) throws Exception {
-        return false;
+    public boolean save(User_password entity) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.save(entity);
+
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
-    public boolean update(User_password dto) throws Exception {
-        return false;
+    public boolean update(User_password entity) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.update(entity);
+
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
-    public boolean delete(String s) throws Exception {
-        return false;
+    public boolean delete(String user) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        User_password userPassword = session.load(User_password.class, user);
+        session.delete(userPassword);
+
+        transaction.commit();
+        session.close();
+        return true;
     }
 }
