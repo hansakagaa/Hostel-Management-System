a3f63c8c4cb1e3fba9f23d8cb62a91569132e43b
Hasii-boy <hansakagaa@gmail.com>
2022-07-02 00:31:12 +0530

    RoomDAOImpl created

--- Hostel_Management_System/src/lk/ijse/Hibernate/dao/custom/impl/RoomDAOImpl.java
+++ Hostel_Management_System/src/lk/ijse/Hibernate/dao/custom/impl/RoomDAOImpl.java
@@ -2,8 +2,12 @@ package lk.ijse.Hibernate.dao.custom.impl;
 
 import lk.ijse.Hibernate.dao.custom.RoomDAO;
 import lk.ijse.Hibernate.entity.Room;
+import lk.ijse.Hibernate.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
     @author : Hasii-boy
@@ -11,36 +15,84 @@ import java.util.ArrayList;
 public class RoomDAOImpl implements RoomDAO {
     @Override
     public boolean exits(String s) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+
+        transaction.commit();
+        session.close();
         return false;
     }
 
     @Override
-    public Room find(String s) throws Exception {
-        return null;
+    public Room find(String id) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        Room room = session.find(Room.class, id);
+
+        transaction.commit();
+        session.close();
+        return room;
     }
 
     @Override
     public ArrayList<Room> getAll() throws Exception {
-        return null;
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<Room> room = session.createQuery("FROM Room ").list();
+
+        transaction.commit();
+        session.close();
+        return (ArrayList<Room>) room;
     }
 
     @Override
-    public boolean save(Room dto) throws Exception {
+    public boolean save(Room entity) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.save(entity);
+
+        transaction.commit();
+        session.close();
         return false;
     }
 
     @Override
-    public boolean update(Room dto) throws Exception {
+    public boolean update(Room entity) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.update(entity);
+
+        transaction.commit();
+        session.close();
         return false;
     }
 
     @Override
-    public boolean delete(String s) throws Exception {
+    public boolean delete(String id) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        Room room = session.load(Room.class, id);
+        session.delete(room);
+
+        transaction.commit();
+        session.close();
         return false;
     }
 
     @Override
     public boolean exitsType(String type) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+
+        transaction.commit();
+        session.close();
         return false;
     }
 }
